d09bbdac1e47074dcb61d8dfefec3e36d50e1c9a
euphoz <alvaro.vergara.f@gmail.com>
2022-06-10 03:46:56 -0400

    agregando navigation y hash

---
+++ src/navigation.js
@@ -0,0 +1,42 @@
+window.addEventListener('DOMContentLoaded', navigator, false);
+window.addEventListener('hashchange', navigator, false);
+
+function navigator(){
+    console.log({location});
+
+    if (location.hash.startsWith('#trends')){
+        trendsPage();
+    } else if (location.hash.startsWith('#search=')){
+        searchPage();
+    }   else if (location.hash.startsWith('#movie=')){
+        moviePage();
+    } else if (location.hash.startsWith('category=')){
+        categoryPage();
+    } else {
+        homePage();
+    }
+    
+    location.hash
+}
+
+function trendsPage(){
+    console.log('TRENDS')
+}
+
+function searchPage(){
+    console.log('SEARCH!')
+}
+
+function moviePage(){
+    console.log('MOVIE!!')
+}
+
+function categoryPage(){
+    console.log('CATEGORY!')
+}
+
+function homePage(){
+    console.log('HOME!!')
+    getTrendingMoviesPreview();
+    getCategegoriesPreview();
+}
